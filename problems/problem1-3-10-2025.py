2ad1e9af4d3e84c1b2eb49dbfc23f126403e6d57
sammi-kitty <samantha.agren@icloud.com>
2025-10-03 11:44:28 +0200

    problem F3 (slumpmässiga tipsrader)

---
+++ problems/problem1-3-10-2025.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import random
+
+PROBABILITIES = ["1","x","2"]
+
+# Problemformulering:
+# Generera tips om 13 rader (1,X,2)
+# Sluta när användaren säger till
+# Bonus: Sannolikheten för de olika värdena ska vara ändringsbar
+
+def main():
+
+    j = 0
+    tips = []
+    while True:
+        list = []
+
+        # Skapa 1 tipsrad
+        for i in range(13):
+            list = list + [PROBABILITIES[random.randint(0, len(PROBABILITIES) - 1)]]
+
+        # Addera tipsraden till de andra tipsraderna
+        tips = tips + [list]
+        print (tips)
+
+        # Fråga användaren om vi vill gå vidare
+        cmd = input("Continue? Y/N")
+        if (cmd.lower() != "y"):
+            break
+
+main()
